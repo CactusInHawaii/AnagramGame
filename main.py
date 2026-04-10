affaf7958a33a298257e89e6bbce5f243ba86731
CactusInHawaii <cstud205403@vyatsu.ru>
2026-04-10 20:46:33 +0300

    Update main.py
    добавлен main.py для запуска игры

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from UI import UI
 
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    app = UI()
+    app.mainloop()
